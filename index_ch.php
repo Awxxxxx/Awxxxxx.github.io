e5e9e6888e3d960faa32d81a2de6b80194c99a2d
Awxxxxx <42958035+Awxxxxx@users.noreply.github.com>
2018-09-06 09:29:47 +0800

    Update index_ch.php

--- index_ch.php
+++ index_ch.php
@@ -4,9 +4,9 @@
     <!--BEGIN HEAD MATE-->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="initial-scale=1.0,width=device-width"/>
-    <meta name="keywords" content="ZHIHUA·WEI,个人简历主页,多版本简历">
-    <meta name="description" content="非你莫属_ZHIHUA.WEI个人简历主页。本项目用于参与码云举办的“非你莫属”为题的Pages简历模板/个性主页征集活动。本项目一共开发了PHP和HTML两种语言版本，前端工程师和后端工程师皆可使用。开源是一种精神！为中国的互联网行业发展献出一份小小的力量。">
-    <meta name="author" content="ZHIHUA·WEI">
+    <meta name="keywords" content="XU PENGYUAN">
+    <meta name="description" content="XXX">
+    <meta name="author" content="XU PENGYUAN">
     <meta name="version" content="1.0.0">
     <!--END HEAD MATE-->
 
@@ -24,7 +24,7 @@
     <!--END HEAD LINK STYLE-->
 
     <!--BEGIN HEAD TITLE-->
-    <title>RESUME-ZHIHUA·WEI</title>
+    <title>XU PENGYUAN - MYSELF</title>
     <!--END HEAD TITLE-->
 </head>
 <body>
@@ -38,17 +38,17 @@
     <?php
     /**
      * ===============================================
-     * Created by ZHIHUA·WEI.
-     * Author: ZHIHUA·WEI <zhihua_wei@foxmail.com>
-     * Date: 2017/12/21 0002
+     * Created by XU PENGYUAN.
+     * Author: XU PENGYUAN <xxx@xxxxxxx.tw>
+     * Date: 2018/19/5 0002
      * Time: Am 11:54
-     * Project: ZHIHUA.WEI Resume
+     * Project: XU PENGYUAN - MYSELF
      * Version: 1.0.0
      * Power: Resume Index
      * ===============================================
      */
 
-    //下面的模块分解成单独的文件，您可以在这里重新排列它们
+    //下面的模块分解成单独的文件，可以在这里重新排列它们
     include('module/chinese/baseinfo.php');
     include('module/chinese/skills.php');
     include('module/chinese/experience.php');
@@ -62,7 +62,7 @@
 <!--END WRAPPER-->
 <!--COPYRIGHT-->
 <div id="copyright">&copy; 2018 - Designed and developed by
-    <a href="http://resume.zhihuawei.xyz/" target="_blank" title="XU PENGYUAN">XU PENGYUAN</a><br>
+    <a href="https://weibo.com/xxxwqnmlgb" target="_blank" title="XU PENGYUAN">XU PENGYUAN</a><br>
     <a href="index.php" title="英文版">英文版</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;中文版&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="index.html" title="HTML版">HTML版</a>
 </div>
 <!--END COPYRIGHT-->
