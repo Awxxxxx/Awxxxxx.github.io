639c4fbc06d66cb7cb10e6f5e5bec8919671d34f
Alex XU <42958035+Awxxxxx@users.noreply.github.com>
2026-04-09 20:19:28 +0800

    Update Package.swift

--- scripts/speech-recognition-spm/Package.swift
+++ scripts/speech-recognition-spm/Package.swift
@@ -1,1 +1,26 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorCommunitySpeechRecognition",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapacitorCommunitySpeechRecognition",
+            targets: ["SpeechRecognitionPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+    ],
+    targets: [
+        .target(
+            name: "SpeechRecognitionPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Plugin",
+            exclude: ["Info.plist", "Plugin.h", "Plugin.m"])
+    ]
+)
 
